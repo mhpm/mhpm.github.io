197168e4daa12ce3652ec928660d839643d848fb
Michelle Perez Morales <michelleeepm@gmail.com>
2017-12-17 17:24:04 -0800

    ready npm run dev no problmes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/Login'
+import Login from '@/components/Login'
 
 Vue.use(Router)
 
@@ -9,7 +9,7 @@ export default new Router({
     {
       path: '/',
       name: 'Login',
-      component: HelloWorld
+      component: Login
     }
   ]
 })
