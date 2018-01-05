5efbc5bf13abf026933c18f33191f888ae820595
Michelle Perez Morales <michelleeepm@gmail.com>
2018-01-05 06:53:55 -0800

    firebase ready

--- public/js/firebase.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    apiKey: "AIzaSyBxlRYB7OidiGaIAiIaIkNhjM-nUB5PQwE",
-    authDomain: "rcm-manager.firebaseapp.com",
-    databaseURL: "https://rcm-manager.firebaseio.com",
-    projectId: "rcm-manager",
-    storageBucket: "rcm-manager.appspot.com",
-    messagingSenderId: "456447116065"
-};
-firebase.initializeApp(config);
